d880f8fc79bfb04bdfe70672caf8d4cc0e44da7f
Stephen Joyv <stepisraelyan@gmail.com>
2024-09-12 00:06:56 +0300

    First Commit

---
+++ demo/src/main/java/demo/demo/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package demo.demo.demo;
+
+import javax.swing.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
